e11e835b160162fdbb4d553f6d9597efc8595bdd
unknown <atul.sanagar@gmail.com>
2023-04-18 19:36:57 +0530

    NewfileforSessiondehydration

---
+++ selenium-tests/src/test/java/dbmi/hms/harvard/edu/authentication/TestSessionStorage.java
@@ -0,0 +1,13 @@
+package dbmi.hms.harvard.edu.authentication;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class TestSessionStorage {
+//	private static WebDriver driver;
+	
+	public static void setItemInSessionStorage(String item, String value) {
+	//	JavascriptExecutor.executeScript(String.format("window.sessionStorage.setItem('%s','%s');", item, value));
+		System.out.print("executing..............");
+	}
+
+	}
